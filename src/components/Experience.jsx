5b513041f7f97858efe0f59ec102c149673eb7d4
hyarlei <hyarlei@Hyarlei>
2024-12-11 09:32:57 -0300

    feat: Resolvido quebra em tela menor

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -115,7 +115,7 @@ const Experience = () => {
       name="experience"
       className="w-full h-screen bg-gradient-to-b from-gray-800 to-black text-white"
     >
-      <div className="max-w-screen-lg p-6 mx-auto flex flex-col justify-center w-full h-full">
+      <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
         <div className="pb-8">
           <p className="text-4xl font-bold inline border-b-4 border-gray-500">
             Experience
@@ -123,7 +123,7 @@ const Experience = () => {
           <p className="py-6">These are the technologies I've worked with</p>
         </div>
 
-        <div className="grid grid-cols-5 sm:grid-cols-3 lg:grid-cols-5 gap-6 text-center py-8 px-4">
+        <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-5 gap-6 text-center py-8 px-4">
           {techs.map(({ id, src, title, style }) => (
             <div
               key={id}
