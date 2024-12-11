b4faf9551e8c5a6328275da33cdd683e786ee61e
hyarlei <hyarlei@Hyarlei>
2024-12-11 09:30:35 -0300

    feat: Resolvido quebra em tela menor

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -16,7 +16,7 @@ const Contact = () => {
   return (
     <div
       name="contact"
-      className="w-full h-scree bg-gradient-to-b to-gray-800 from-black text-white"
+      className="w-full bg-gradient-to-b to-gray-800 from-black text-white"
     >
       <div className="flex flex-col p-4 justify-center max-w-screen-lg mx-auto h-full">
         <div className="pb-8">
--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -115,15 +115,15 @@ const Experience = () => {
       name="experience"
       className="w-full h-screen bg-gradient-to-b from-gray-800 to-black text-white"
     >
-      <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
-        <div className="pb-8">
+      <div className="max-w-screen-lg p-6 mx-auto flex flex-col justify-center w-full h-full">
+        <div>
           <p className="text-4xl font-bold inline border-b-4 border-gray-500">
             Experience
           </p>
           <p className="py-6">These are the technologies I've worked with</p>
         </div>
 
-        <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-5 gap-6 text-center py-8 px-4">
+        <div className="grid grid-cols-5 sm:grid-cols-3 lg:grid-cols-5 gap-6 text-center py-8 px-4">
           {techs.map(({ id, src, title, style }) => (
             <div
               key={id}
